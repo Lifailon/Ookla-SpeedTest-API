e2cf73f86c5517b5a0b0af10a5f7c97244482dc5
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-05-02 16:58:20 +0300

    Add files via upload

--- Deploy-Invoke-SpeedTest.ps1
+++ Deploy-Invoke-SpeedTest.ps1
@@ -1,15 +1,5 @@
-$path_psm = ($env:PSModulePath.Split(";")[0])+"\Invoke-SpeedTest\1.0\Invoke-SpeedTest.psm1"
-$path_psd = ($env:PSModulePath.Split(";")[0])+"\Invoke-SpeedTest\1.0\Invoke-SpeedTest.psd1"
-
+$path_psm = ($env:PSModulePath.Split(";")[0])+"\Invoke-SpeedTest\Invoke-SpeedTest.psm1"
 if (!(Test-Path $path_psm)) {
     New-Item $path_psm -ItemType File -Force
 }
-
-if (!(Test-Path $path_psd)) {
-    New-Item $path_psd -ItemType File -Force
-}
-
-irm https://raw.githubusercontent.com/Lifailon/Ookla-SpeedTest-API/rsa/Invoke-SpeedTest/1.0/Invoke-SpeedTest.psm1 | 
-Out-File $path_psm -Force
-irm https://raw.githubusercontent.com/Lifailon/Ookla-SpeedTest-API/rsa/Invoke-SpeedTest/1.0/Invoke-SpeedTest.psd1 | 
-Out-File $path_psd -Force
+irm https://raw.githubusercontent.com/Lifailon/Ookla-SpeedTest-API/rsa/Invoke-SpeedTest/Invoke-SpeedTest.psm1 | Out-File $path_psm -Force
