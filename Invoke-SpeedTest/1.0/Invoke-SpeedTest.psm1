fe1ea9a3be97a70a22cd4ab5817093342a98b5fd
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-05-02 16:39:06 +0300

    Add files via upload

--- Invoke-SpeedTest/1.0/Invoke-SpeedTest.psm1
+++ Invoke-SpeedTest/1.0/Invoke-SpeedTest.psm1
@@ -2,6 +2,7 @@ function Invoke-SpeedTest {
 <#
 .SYNOPSIS
 Module for creating metrics measurements Internet speed to mode cli (no use dependencies) and output to log file
+Data collection resource: speedtest.net (dev Ookla)
 Using native API method (via InternetExplorer) for web function start
 Using REST API GET method (via Invoke-RestMethod) for parsing JSON report
 Works in PSVersion 5.1 (PowerShell 7.3 not supported)
