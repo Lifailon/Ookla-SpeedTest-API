c8401c7e6db0cc2d5bd63838e260fd790cbfe4d9
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-07-13 20:12:11 +0300

    debug timestamp utc 0 (if +)

--- Scripts/SpeedTestTo-InfluxDB.ps1
+++ Scripts/SpeedTestTo-InfluxDB.ps1
@@ -8,6 +8,7 @@ $table = "speedtest"	 # Measurement/Table name
 $ipp   = $ip+":"+$p
 $url   = "http://$ipp/write?db=$db"
 $hn    = $ENV:COMPUTERNAME
+$tz    = (Get-TimeZone).BaseUtcOffset.TotalMinutes
 
 while ($True) {
     $test  = Invoke-SpeedTest
@@ -15,7 +16,7 @@ while ($True) {
     $up    = $test.upload
     $ping  = $test.latency
 
-    $unixtime = (New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalSeconds
+	$unixtime  = (New-TimeSpan -Start (Get-Date "01/01/1970") -End ((Get-Date).AddMinutes(-$tz))).TotalSeconds # UTC 0 (if +)
     $timestamp = ([string]$unixtime -replace "\..+") + "000000000"
 
     Invoke-RestMethod -Method POST -Uri $url -Body "$table,host=$hn download=$down,upload=$up,ping=$ping $timestamp"
